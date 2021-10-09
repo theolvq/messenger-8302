af97313a928c0e62b20ace6f076a0a8909418dab
Theo Leveque <theo.leveque@gmail.com>
2021-10-09 10:57:12 -0700

    fixed MUI deprecated properties so console is less cluttered

--- client/src/components/Sidebar/BadgeAvatar.js
+++ client/src/components/Sidebar/BadgeAvatar.js
@@ -6,21 +6,21 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
   profilePic: {
     height: 44,
-    width: 44
+    width: 44,
   },
   badge: {
     height: 13,
     width: 13,
     borderRadius: "50%",
     border: "2px solid white",
-    backgroundColor: "#D0DAE9"
+    backgroundColor: "#D0DAE9",
   },
   online: {
-    backgroundColor: "#1CED84"
+    backgroundColor: "#1CED84",
   },
   sidebar: {
-    marginLeft: 17
-  }
+    marginLeft: 17,
+  },
 }));
 
 const UserAvatar = (props) => {
@@ -33,8 +33,11 @@ const UserAvatar = (props) => {
         classes={{ badge: `${classes.badge} ${online && classes.online}` }}
         variant="dot"
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        overlap="circle">
-        <Avatar alt={username} src={photoUrl} className={classes.profilePic}></Avatar>
+        overlap="circular">
+        <Avatar
+          alt={username}
+          src={photoUrl}
+          className={classes.profilePic}></Avatar>
       </Badge>
     </Box>
   );
