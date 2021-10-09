dcb50a933539ce20bd75faf86e857807bbd8844d
Theo Leveque <theo.leveque@gmail.com>
2021-10-09 10:49:33 -0700

    fixed sending message bug: added immutability in reducerFunction > addMessageToStore and added async/await in thunkCreator > postMessage

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -13,9 +13,10 @@ export const addMessageToStore = (state, payload) => {
 
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      const convoCopy = { ...convo };
+      convoCopy.messages = [...convoCopy.messages, message];
+      convoCopy.latestMessageText = message.text;
+      return convoCopy;
     } else {
       return convo;
     }
