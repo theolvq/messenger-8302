a56c4c3eef147355d0ae292b5e9aadfee300431c
Theo Leveque <theo.leveque@gmail.com>
2021-10-10 20:12:49 -0700

    rough layout done according to spec, needs some fine tuning

--- client/src/Login.js
+++ client/src/Login.js
@@ -8,6 +8,8 @@ import {
   Button,
   FormControl,
   TextField,
+  InputAdornment,
+  Link,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { login } from "./store/utils/thunkCreators";
@@ -24,13 +26,53 @@ const useStyles = makeStyles(() => ({
       "linear-gradient(to bottom, #3A8DFFD9, #86B9FFD9),  url(https://res.cloudinary.com/diswlgneg/image/upload/v1633815148/bg-img_i2xyym.png) ",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
-    minHeight: "100vh",
+    height: "100vh",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+    gap: "2.5rem",
     color: "white",
     textAlign: "center",
+    width: "45%",
+  },
+  main: {
+    height: "100vh",
+    width: "55%",
+    justifyContent: "center",
+  },
+  top: {
+    padding: "1.875rem 2.625rem",
+    alignSelf: "flex-start",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  lightText: {
+    color: "#B0B0B0",
+  },
+  loginBtn: {
+    fontFamily: "Montserrat",
+    margin: "2rem",
+  },
+  btn__secondary: {
+    boxShadow: "0px 3px 5px -3px #3A8DFF66",
+  },
+  form: {
+    maxWidth: "358px",
+    margin: "0 auto",
+  },
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "2.375rem",
+  },
+  formTitle: {
+    marginBottom: "2rem",
+    fontWeight: 600,
+  },
+  inputContainer: {
+    gap: "2.625rem",
   },
 }));
 
@@ -42,6 +84,7 @@ const Login = (props) => {
   const handleLogin = async (event) => {
     event.preventDefault();
     const username = event.target.username.value;
+    const emailAddress = event.target.emailAddress.value;
     const password = event.target.password.value;
 
     await login({ username, password });
@@ -59,39 +102,64 @@ const Login = (props) => {
             src="https://res.cloudinary.com/diswlgneg/image/upload/v1633837554/bubble_xssqyw.svg"
             alt="speech bubble icon"
           />
-          <h3>
-            Converse with anyone, <br /> in any language.
-          </h3>
+          <Typography variant="h2" className={classes.slogan}>
+            Converse with anyone <br /> with any language.
+          </Typography>
         </Grid>
-        <Grid container item>
-          <Grid container item>
-            <Typography>Don't have an account?</Typography>
-            <Button onClick={() => history.push("/register")}>
+        <Grid container className={classes.main}>
+          <Grid container className={classes.top}>
+            <Typography variant="body2" className={classes.lightText}>
+              Don't have an account?
+            </Typography>
+            <Button
+              size="large"
+              color="primary"
+              className={classes.btn__secondary}
+              onClick={() => history.push("/register")}>
               Create account
             </Button>
           </Grid>
-          <form onSubmit={handleLogin}>
-            <Grid>
-              <Grid>
-                <FormControl margin="normal" required>
+          <form onSubmit={handleLogin} className={classes.form}>
+            <Typography variant="h2" className={classes.formTitle}>
+              Welcome back!
+            </Typography>
+            <Grid container className={classes.formContainer}>
+              <Grid container item className={classes.inputContainer}>
+                <FormControl required fullWidth>
                   <TextField
-                    aria-label="username"
-                    label="Username"
-                    name="username"
+                    aria-label="E-mail address"
+                    label="E-mail address"
+                    name="emailAddress"
                     type="text"
                   />
                 </FormControl>
+                <FormControl required fullWidth>
+                  <TextField
+                    label="Password"
+                    aria-label="password"
+                    type="password"
+                    name="password"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <Button
+                            aria-label="Password recovery"
+                            color="primary">
+                            Forgot?
+                          </Button>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </FormControl>
               </Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  label="password"
-                  aria-label="password"
-                  type="password"
-                  name="password"
-                />
-              </FormControl>
-              <Grid>
-                <Button type="submit" variant="contained" size="large">
+              <Grid item>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  className={classes.loginBtn}>
                   Login
                 </Button>
               </Grid>
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -7,18 +7,20 @@ export const theme = createMuiTheme({
     button: {
       textTransform: "none",
       letterSpacing: 0,
-      fontWeight: "bold"
-    }
+    },
+    h2: {
+      fontSize: "1.625rem",
+    },
   },
   overrides: {
     MuiInput: {
       input: {
-        fontWeight: "bold"
-      }
-    }
+        fontWeight: "bold",
+      },
+    },
   },
   palette: {
     primary: { main: "#3A8DFF" },
-    secondary: { main: "#B0B0B0" }
-  }
+    secondary: { main: "#B0B0B0" },
+  },
 });
