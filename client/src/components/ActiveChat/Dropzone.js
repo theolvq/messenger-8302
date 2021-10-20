655c837c68ab1d9a9779758cce568a8c94828fcc
Theo Leveque <theo.leveque@gmail.com>
2021-10-20 14:00:24 -0700

    simplified and optimized onDrop function in Dropzone to use Promise.all

--- client/src/components/ActiveChat/Dropzone.js
+++ client/src/components/ActiveChat/Dropzone.js
@@ -1,8 +1,8 @@
 import React, { useCallback } from "react";
-import axios from "axios";
 import { Button, makeStyles } from "@material-ui/core";
 import { Image } from "@material-ui/icons";
 import { useDropzone } from "react-dropzone";
+import { uploadImage } from "../../store/utils/thunkCreators";
 
 const useStyles = makeStyles(() => ({
   uploadButton: {
@@ -10,40 +10,15 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const PRESET = process.env.REACT_APP_CLOUDINARY_PRESET;
-
 const Dropzone = (props) => {
   const classes = useStyles();
   const { setAttachments } = props;
 
-  const uploadImage = async (image) => {
-    try {
-      const formData = new FormData();
-      formData.append("file", image);
-      formData.append("upload_preset", PRESET);
-      formData.append("folder", "messenger-hatchways");
-
-      const { data } = await axios.post(
-        "https://api.cloudinary.com/v1_1/daawascript/upload",
-        formData
-      );
-      return data.secure_url;
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   const onDrop = useCallback(
-    (images) => {
-      images.forEach((image) => {
-        const reader = new FileReader();
-
-        reader.onload = async () => {
-          const url = await uploadImage(image);
-          setAttachments((prev) => prev.concat(url));
-        };
-        reader.readAsArrayBuffer(image);
-      });
+    async (images) => {
+      const promises = images.map(async (image) => await uploadImage(image));
+      const urls = await Promise.all(promises);
+      setAttachments((prev) => prev.concat(urls));
     },
     [setAttachments]
   );
